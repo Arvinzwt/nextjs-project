0ddb131051f1150fd76f2658e3ee351d8b9d081e
zhangwentao <arvinzhwt@qq.com>
2024-01-30 16:49:26 +0800

    nextjs:10 add search and pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -1,14 +1,24 @@
 'use client';
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
+import {ArrowLeftIcon, ArrowRightIcon} from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+import {generatePagination} from '@/app/lib/utils';
+import {usePathname, useSearchParams} from "next/navigation";
 
-export default function Pagination({ totalPages }: { totalPages: number }) {
+export default function Pagination({totalPages}: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
 
   // const allPages = generatePagination(currentPage, totalPages);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
 
   return (
     <>
@@ -53,11 +63,11 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 }
 
 function PaginationNumber({
-  page,
-  href,
-  isActive,
-  position,
-}: {
+                            page,
+                            href,
+                            isActive,
+                            position,
+                          }: {
   page: number | string;
   href: string;
   position?: 'first' | 'last' | 'middle' | 'single';
@@ -84,10 +94,10 @@ function PaginationNumber({
 }
 
 function PaginationArrow({
-  href,
-  direction,
-  isDisabled,
-}: {
+                           href,
+                           direction,
+                           isDisabled,
+                         }: {
   href: string;
   direction: 'left' | 'right';
   isDisabled?: boolean;
@@ -104,9 +114,9 @@ function PaginationArrow({
 
   const icon =
     direction === 'left' ? (
-      <ArrowLeftIcon className="w-4" />
+      <ArrowLeftIcon className="w-4"/>
     ) : (
-      <ArrowRightIcon className="w-4" />
+      <ArrowRightIcon className="w-4"/>
     );
 
   return isDisabled ? (
